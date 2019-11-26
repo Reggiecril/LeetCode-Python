2fd9a7d6187ad32c10c447bb57a2bb15ae77def3
谢云丞 <xieyuncheng@xieyunchengdeMacBook-Pro.local>
2019-11-26 18:55:33 +0800

    LinkedListCycle

---
+++ src/LinkedListCycle.java
@@ -0,0 +1,12 @@
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast=head,slow=head;
+        while(fast!=null&&fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(slow==fast)
+                return true;
+        }
+        return false;
+    }
+}
